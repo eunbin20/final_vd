b8c368b1423ff1ba380f0961e5d5119fb406cbc8
eunbin <dmsqls__0228@naver.com>
2020-06-24 22:40:49 +0900

    C

--- accounts/views.py
+++ accounts/views.py
@@ -38,30 +38,6 @@ def signup(request):
         form = UserForm()
     return render(request, 'accounts/signup.html', {'form': form})
 
-
-# def signup(request):
-#     if request.method == "POST":
-#         if request.POST['password1'] == request.POST['password2']:
-#             user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
-#             auth.login(request, user)
-#         return redirect('home')
-#     return render(request, 'accounts/signup.html')
-
-
-# def login(request):
-#     if request.method == "POST":
-#         username = request.POST['username']
-#         password = request.POST['password']
-#         user=auth.authenticate(request, username=username, password=password)
-#         if user is not None:
-#             auth.login(request, user)
-#             return redirect('show')
-#         else:
-#             return render(request, 'accounts/login.html', {'error': 'username or password is incorrect.'})
-#     else:
-#         return render(request, 'accounts/login.html')
-
-
 def logout(request):
     auth.logout(request)
     return redirect('/')
